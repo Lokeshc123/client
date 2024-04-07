7b5395679757a421f59a60ef6eaf3ad5c9c104de
Lokesh Chauhan <68843786+Lokeshc123@users.noreply.github.com>
2024-04-07 07:28:30 +0530

    hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 import Navbar from "./components/navbar/Navbar";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React from "react";
 function App() {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Products />
     </div>
   );
 }
